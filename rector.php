0efb17308602ab276c4fa747258e976721638f4e
Paul Mitchum <paul@mile23.com>
2023-02-17 12:55:44 -0800

    fix rector.php

--- rector.php
+++ rector.php
@@ -6,12 +6,12 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-  $rectorConfig->paths([
+    $rectorConfig->paths([
     __DIR__ . '/src',
     __DIR__ . '/test',
-  ]);
+    ]);
 
-  $rectorConfig->sets([
+    $rectorConfig->sets([
     LevelSetList::UP_TO_PHP_74,
-  ]);
+    ]);
 };
